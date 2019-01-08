7446ec3f9888146d8ff25befc2bfb96cca7c9e02
Daniel Alberto Larios Torres <daniellarios@MacBook-Air-de-Daniel.local>
2019-01-08 15:14:20 -0600

    First commit

---
+++ src/categories/components/categories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Category from './category';
+import './categories.css';
+import Search from '../../widgets/containers/search';
+import Media from '../../playlist/components/media'
+
+function Categories(props) {
+  console.log(props)
+  return (
+    <div className="Categories">
+      <Search />
+      {
+        props.search.map((item) => {
+          return <Media {...item} key={item.id} /> 
+        })
+      }
+      {
+        props.categories.map((item) =>{
+          return (
+            <Category
+              key={item.id}
+              {...item}
+              handleOpenModal={props.handleOpenModal}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Categories
